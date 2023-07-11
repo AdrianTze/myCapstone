047eb8f4337400ad5cfdedc658df346da73e02bd
AdrianTze <adriantze@gmail.com>
2023-07-12 01:17:14 +1000

    style and layout visual elements in UserReview component based on Figma wireframe

--- src/MainComponents/UserReview.js
+++ src/MainComponents/UserReview.js
@@ -1,14 +1,29 @@
-import { Avatar, Card } from "@chakra-ui/react";
+import { Avatar, Card, HStack, Heading, Text } from "@chakra-ui/react";
 
 const UserRating = (props) => {
   const { imgSrc, fname, rating, review } = props.data;
 
   return (
-    <Card maxW={"3xs"}>
-      <Avatar name={fname} src={imgSrc} size={"xl"} />
-      <h3>{fname}</h3>
-      <h3>{rating}</h3>
-      <p>{review}</p>
+    <Card
+      bg={"secondary.300"}
+      maxW={"3xs"}
+      align={"center"}
+      justify={"center"}
+      p={"5"}
+    >
+      <Avatar name={fname} src={imgSrc} bg={"secondary.200"} size={"xl"} />
+      <HStack>
+        <Heading as={"h4"} fontSize={"lg"} p={"2"}>
+          {fname}
+        </Heading>
+        <Heading as={"h4"} fontSize={"2xl"} p={"2"}>
+          {rating}
+        </Heading>
+      </HStack>
+
+      <Text fontStyle={"italic"} fontWeight={"medium"} fontSize={"sm"}>
+        {review}
+      </Text>
     </Card>
   );
 };
