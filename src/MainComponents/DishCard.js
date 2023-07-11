73c8aa69e97b879c8295d06f329e579c7e0faf4d
AdrianTze <adriantze@gmail.com>
2023-07-12 01:14:38 +1000

    style and layout visual elements in DishCard based on Figma wireframe

--- src/MainComponents/DishCard.js
+++ src/MainComponents/DishCard.js
@@ -1,20 +1,37 @@
 import Icon from "@mdi/react";
 import { mdiMoped } from "@mdi/js";
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 
 const DishCard = function (props) {
   const { imgPth, imgAlt, name, price, description } = props.data;
 
   return (
     <>
-      <Card maxW={"sm"}>
+      <Card maxW={"280"} bg={"secondary.300"}>
         <CardBody>
-          <Image src={imgPth} alt={imgAlt} borderRadius="lg" />
-          <Heading size={"md"}>{name}</Heading>
-          <Text>{price}</Text>
-          <Text>{description}</Text>
-          <Text>Order a delivery</Text>
-          <Icon path={mdiMoped} size={1} />
+          <Image
+            src={imgPth}
+            alt={imgAlt}
+            borderRadius="lg"
+            width={"lg"}
+            height={"6xs"}
+          />
+          <HStack justifyContent={"space-between"}>
+            <Heading fontSize={"2xl"} py={"3"}>
+              {name}
+            </Heading>
+            <Text fontSize={"xl"} color={"secondary.100"} fontWeight={"bold"}>
+              {price}
+            </Text>
+          </HStack>
+
+          <Text fontSize={"sm"} paddingBottom={"4"}>
+            {description}
+          </Text>
+          <HStack>
+            <Text>Order a delivery</Text>
+            <Icon path={mdiMoped} size={1} color={"green"} />
+          </HStack>
         </CardBody>
       </Card>
     </>
