9f989e55ff671c2c2d6847785d9e26dee35cc5f5
AdrianTze <adriantze@gmail.com>
2023-07-12 01:16:06 +1000

    style and layout visual elements in DishCard based on Figma wireframe

--- src/MainComponents/Nav.js
+++ src/MainComponents/Nav.js
@@ -1,32 +1,46 @@
+import { ListItem, UnorderedList } from "@chakra-ui/react";
 import logo from "../assets/Logo.svg";
 
 const Nav = () => {
+  const handleClick = (anchor) => () => {
+    const className = `${anchor}`;
+    const element = document.getElementsByClassName(className)[0];
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
-    <>
-      <img src={logo} alt="Little Lemon Logo" width={"300px"} />
-      <nav>
-        <ul>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>About</a>
-          </li>
-          <li>
-            <a>Menu</a>
-          </li>
-          <li>
-            <a>Reservation</a>
-          </li>
-          <li>
-            <a>Order Online</a>
-          </li>
-          <li>
-            <a>Login</a>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <UnorderedList>
+        <ListItem>
+          <img src={logo} alt="Little Lemon Logo" width={"200px"} />
+        </ListItem>
+        <ListItem>
+          <a href="#Home">Home</a>
+        </ListItem>
+        <ListItem>
+          <a href="#About" onClick={handleClick("aboutus")}>
+            About
+          </a>
+        </ListItem>
+        <ListItem>
+          <a href="#Menu">Menu</a>
+        </ListItem>
+        <ListItem>
+          <a href="#Reservation">Reservation</a>
+        </ListItem>
+        <ListItem>
+          <a href="#Order">Order Online</a>
+        </ListItem>
+        <ListItem>
+          <a href="#Login">Login</a>
+        </ListItem>
+      </UnorderedList>
+    </nav>
   );
 };
 
