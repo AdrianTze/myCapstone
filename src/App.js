28d0f43ef78d39d10bf48cfb46181fdf0faff782
AdrianTze <adriantze@gmail.com>
2023-07-11 15:31:28 +1000

    added CSS Grid to the web page layout

--- src/App.js
+++ src/App.js
@@ -10,21 +10,48 @@ import Footer from "./MainComponents/Footer";
 // import Payment from "./ReservationComponents/Payment";
 // import Reservation from "./ReservationComponents/Reservation";
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
+import Theme from "./Theme";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={Theme}>
       <Fragment className="App">
         <body>
-          <Nav></Nav>
-          <Header></Header>
-          <main>
-            <Special></Special>
-            <Testimonial></Testimonial>
-            <AboutUs></AboutUs>
-          </main>
-          <Footer></Footer>
+          <Grid
+            templateAreas={`"nav"
+                  "header"
+                  "special"
+                  "testimonials"
+                  "about"
+                  "footer"`}
+            gridTemplateRows={"6rem 1fr 1fr 1fr 1fr 25rem"}
+            gridTemplateColumns={"1fr"}
+            h="-webkit-fit-content"
+            gap="0"
+            fontWeight="bold"
+          >
+            <GridItem pl="2" bg="white.300" area={"nav"}>
+              <Nav></Nav>
+            </GridItem>
+            <main>
+              <GridItem pl="2" bg="primary.100" area={"header"}>
+                <Header></Header>
+              </GridItem>
+              <GridItem pl="2" bg="white.300" area={"special"}>
+                <Special></Special>
+              </GridItem>
+              <GridItem pl="2" bg="primary.100" area={"testimonials"}>
+                <Testimonial></Testimonial>
+              </GridItem>
+              <GridItem pl="2" bg="secondary.300" area={"about"}>
+                <AboutUs></AboutUs>
+              </GridItem>
+            </main>
+            <GridItem pl="2" bg="secondary.200" area={"footer"}>
+              <Footer></Footer>
+            </GridItem>
+          </Grid>
         </body>
       </Fragment>
     </ChakraProvider>
