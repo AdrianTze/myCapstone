83950bdf7c1f49d3b080509e1eaefb22b211a45a
AdrianTze <adriantze@gmail.com>
2023-07-12 01:14:03 +1000

    style and layout visual elements in About component

--- src/MainComponents/AboutUs.js
+++ src/MainComponents/AboutUs.js
@@ -1,26 +1,48 @@
-import { Heading, Image, Text } from "@chakra-ui/react";
+import { Heading, Text, VStack } from "@chakra-ui/react";
 import OwnerImage from "../assets/Mario and Adrian A.jpg";
 import OwnerImage2 from "../assets/Mario and Adrian b.jpg";
 
 const AboutUs = () => {
   return (
     <section className="aboutus">
-      <Heading as={"h1"} size={"3xl"}>
-        Little Lemon
-      </Heading>
-      <Heading as={"h2"} size={"2xl"}>
-        Chicago
-      </Heading>
-      <Text>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged.
-      </Text>
-      <Image src={OwnerImage} />
-      <Image src={OwnerImage2} />
+      <VStack p={"5"} alignItems={"start"} maxW={"lg"}>
+        <Heading
+          as={"h1"}
+          color={"primary.100"}
+          fontWeight={"medium"}
+          size={"3xl"}
+        >
+          Little Lemon
+        </Heading>
+        <Heading
+          as={"h2"}
+          color={"primary.200"}
+          fontWeight={"medium"}
+          size={"2xl"}
+        >
+          Chicago
+        </Heading>
+        <Text>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </Text>
+      </VStack>
+      <div className="container">
+        <img
+          className="stackImage"
+          src={OwnerImage2}
+          alt="Adrian and Mario are having a conversation in the kitchen"
+        />
+        <img
+          className="stackImage"
+          src={OwnerImage}
+          alt="Adrian and Mario are smiling while looking at their hand-made dish"
+        />
+      </div>
     </section>
   );
 };
