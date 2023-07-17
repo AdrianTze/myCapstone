6221198fda7473af8701c71393f46530af3da219
AdrianTze <adriantze@gmail.com>
2023-07-17 15:54:57 +1000

    add visual elements and styles to contact, payment and reservation components

--- src/ReservationComponents/ContactForm.js
+++ src/ReservationComponents/ContactForm.js
@@ -1,9 +1,135 @@
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  HStack,
+  Heading,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
 import logo from "../assets/Logo.svg";
 
 const ContactForm = () => {
   return (
     <section className="contactform">
-      <img src={logo} alt="Little Lemon Logo" width={"300px"} />
+      <HStack wrap={"wrap"} justifyContent={"space-evenly"} pb={"10"}>
+        <img src={logo} alt="Little Lemon Logo" width={"300rem"} />
+        <Heading as={"h1"} color={"secondary.400"} fontWeight={"semi-bold"}>
+          Contact Form
+        </Heading>
+      </HStack>
+
+      <FormControl isRequired isInvalid={false} maxW={"5xl"} px={"20"}>
+        <HStack>
+          <FormLabel
+            htmlFor="firstName"
+            color={"secondary.400"}
+            fontSize={"lg"}
+          >
+            First Name
+          </FormLabel>
+          <FormErrorMessage pb={"4"}>Required</FormErrorMessage>
+        </HStack>
+        <Input
+          variant={"filled"}
+          placeholder="*** First Name"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+          type="text"
+        />
+      </FormControl>
+
+      <FormControl isRequired isInvalid={false} maxW={"5xl"} px={"20"}>
+        <HStack>
+          <FormLabel htmlFor="lastName" color={"secondary.400"} fontSize={"lg"}>
+            Last Name
+          </FormLabel>
+          <FormErrorMessage pb={"4"}>Required</FormErrorMessage>
+        </HStack>
+        <Input
+          variant={"filled"}
+          placeholder="*** Last Name"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+          type="text"
+        />
+      </FormControl>
+
+      <FormControl isRequired isInvalid={false} maxW={"5xl"} px={"20"}>
+        <HStack>
+          <FormLabel htmlFor="phone" color={"secondary.400"} fontSize={"lg"}>
+            Phone Number
+          </FormLabel>
+          <FormErrorMessage pb={"4"}>Required</FormErrorMessage>
+        </HStack>
+        <Input
+          variant={"filled"}
+          placeholder="*** Phone Number"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+          type="text"
+        />
+      </FormControl>
+
+      <FormControl isRequired isInvalid={false} maxW={"5xl"} px={"20"}>
+        <HStack>
+          <FormLabel htmlFor="email" color={"secondary.400"} fontSize={"lg"}>
+            Email
+          </FormLabel>
+          <FormErrorMessage pb={"4"}>Required</FormErrorMessage>
+        </HStack>
+        <Input
+          variant={"filled"}
+          placeholder="*** johndoe@gmail.com"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+          type="email"
+        />
+        <FormHelperText>We'll never share your email.</FormHelperText>
+      </FormControl>
+
+      <FormControl isRequired isInvalid={false} maxW={"5xl"} px={"20"}>
+        <HStack>
+          <FormLabel htmlFor="password" color={"secondary.400"} fontSize={"lg"}>
+            Password
+          </FormLabel>
+          <FormErrorMessage pb={"4"}>Required</FormErrorMessage>
+        </HStack>
+        <Input
+          variant={"filled"}
+          placeholder="*** Password"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+          type="password"
+        />
+      </FormControl>
+
+      <FormControl maxW={"5xl"} px={"20"}>
+        <FormLabel
+          optionalIndicator
+          htmlFor="password"
+          color={"secondary.400"}
+          fontSize={"lg"}
+        >
+          Additional information we should know
+        </FormLabel>
+        <Textarea
+          variant={"filled"}
+          placeholder="Special Request / Dietary Requirements"
+          borderColor={"secondary.400"}
+          focusBorderColor="primary.200"
+        />
+      </FormControl>
+      <Button
+        alignSelf={"center"}
+        maxW={"lg"}
+        color={"secondary.400"}
+        bg={"primary.200"}
+      >
+        Continue
+      </Button>
     </section>
   );
 };
--- src/ReservationComponents/Reservation.js
+++ src/ReservationComponents/Reservation.js
@@ -1,5 +1,184 @@
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Heading,
+  Input,
+  Radio,
+  RadioGroup,
+  Select,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Stack,
+  Text,
+  Tooltip,
+  VStack,
+} from "@chakra-ui/react";
+import restaurantImage from "../assets/restaurant.jpg";
+import React from "react";
+
 const Reservation = () => {
-  return <section className="reservation"></section>;
+  const [sliderValue, setSliderValue] = React.useState(4);
+  const [showTooltip, setShowTooltip] = React.useState(false);
+
+  return (
+    <section className="reservation">
+      <VStack alignItems={"flex-start"}>
+        <Heading
+          as={"h1"}
+          color={"primary.200"}
+          fontWeight={"medium"}
+          size={"3xl"}
+        >
+          Reservation
+        </Heading>
+
+        <Heading
+          as={"h2"}
+          color={"secondary.300"}
+          fontWeight={"medium"}
+          size={"xl"}
+        >
+          Find a table for any occasion
+        </Heading>
+
+        <img
+          src={restaurantImage}
+          alt="A restaurant chef sprinkling garnish to the cuisine"
+          width={"350rem"}
+        />
+      </VStack>
+      <VStack>
+        <FormControl w={"md"} px={"20"}>
+          <FormLabel
+            htmlFor="firstName"
+            color={"secondary.300"}
+            fontSize={"lg"}
+          >
+            Date
+          </FormLabel>
+          <Input
+            variant={"filled"}
+            placeholder="Enter date"
+            borderColor={"secondary.400"}
+            focusBorderColor="primary.200"
+            type="date"
+          />
+          <FormErrorMessage></FormErrorMessage>
+        </FormControl>
+        <FormControl w={"md"} px={"20"}>
+          <FormLabel
+            htmlFor="firstName"
+            color={"secondary.300"}
+            fontSize={"lg"}
+          >
+            Time
+          </FormLabel>
+          <Select
+            variant={"filled"}
+            borderColor={"secondary.400"}
+            focusBorderColor="primary.200"
+            type="date"
+          >
+            <option value="17:00">17:00</option>
+            <option value="18:00">18:00</option>
+            <option value="19:00">19:00</option>
+            <option value="20:00">20:00</option>
+            <option value="21:00">21:00</option>
+          </Select>
+          <FormErrorMessage></FormErrorMessage>
+        </FormControl>
+
+        <FormControl w={"md"} px={"20"}>
+          <HStack>
+            <FormLabel
+              htmlFor="firstName"
+              color={"secondary.300"}
+              fontSize={"lg"}
+            >
+              Number of Diners
+            </FormLabel>
+            <Text pb={"2"} pl={"10"} color={"secondary.300"} fontSize={"2xl"}>
+              {sliderValue}
+            </Text>
+          </HStack>
+          <Slider
+            defaultValue={4}
+            min={2}
+            max={10}
+            step={1}
+            onChange={(v) => setSliderValue(v)}
+            onMouseEnter={(e) => setShowTooltip(true)}
+            onMouseLeave={(e) => setShowTooltip(false)}
+          >
+            <SliderTrack bg="secondary.200">
+              <Box position="relative" right={10} />
+              <SliderFilledTrack bg="primary.200" />
+            </SliderTrack>
+
+            <Tooltip
+              hasArrow
+              bg="secondary.300"
+              color="primary.100"
+              placement="top"
+              isOpen={showTooltip}
+              label={`${sliderValue}`}
+            >
+              <SliderThumb boxSize={6} />
+            </Tooltip>
+          </Slider>
+          <FormErrorMessage></FormErrorMessage>
+        </FormControl>
+
+        <FormControl w={"md"} px={"20"}>
+          <FormLabel
+            htmlFor="firstName"
+            color={"secondary.300"}
+            fontSize={"lg"}
+          >
+            Occasion
+          </FormLabel>
+          <Select
+            variant={"filled"}
+            borderColor={"secondary.400"}
+            focusBorderColor="primary.200"
+            type="date"
+          >
+            <option value="Birthday">Birthday</option>
+            <option value="Engagement">Engagement</option>
+            <option value="Anniversary">Anniversary</option>
+            <option value="Other">Other</option>
+          </Select>
+          <FormErrorMessage></FormErrorMessage>
+        </FormControl>
+
+        <FormControl w={"md"} px={"20"}>
+          <FormLabel
+            htmlFor="firstName"
+            color={"secondary.300"}
+            fontSize={"lg"}
+          >
+            Seating Options
+          </FormLabel>
+          <RadioGroup>
+            <Stack direction="column">
+              <Radio colorScheme="primary" value="Standard">
+                <Text color={"secondary.300"}>Standard</Text>
+              </Radio>
+              <Radio colorScheme="primary" value="Outside">
+                <Text color={"secondary.300"}>Outside</Text>
+              </Radio>
+            </Stack>
+          </RadioGroup>
+          <FormErrorMessage></FormErrorMessage>
+        </FormControl>
+      </VStack>
+    </section>
+  );
 };
 
 export default Reservation;
