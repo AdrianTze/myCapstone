ba522e35e1a9533dd24c257c1fa09c8761df4caf
AdrianTze <adriantze@gmail.com>
2023-08-06 02:18:53 +0800

    Wrapped the app component in AlertProvider and added Alert component in

--- src/App.js
+++ src/App.js
@@ -13,73 +13,78 @@ import Reservation from "./ReservationComponents/Reservation";
 import { ChakraProvider, Grid, GridItem } from "@chakra-ui/react";
 import Theme from "./Theme";
 import { Route, Routes } from "react-router-dom";
+import { AlertProvider } from "./contextAPI/alertContext";
+import Alert from "./MainComponents/Alert";
 
 function App() {
   return (
     <ChakraProvider theme={Theme}>
-      <Fragment className="App">
-        <body>
-          <Grid
-            templateAreas={`"nav"
+      <AlertProvider>
+        <Fragment className="App">
+          <body>
+            <Grid
+              templateAreas={`"nav"
                   "header"
                   "special"
                   "testimonials"
                   "about"
                   "footer"`}
-            gridTemplateRows={"auto auto 1fr 1fr 1fr auto"}
-            gridTemplateColumns={"1fr"}
-            h="-webkit-fit-content"
-            gap="0"
-            fontWeight="bold"
-          >
-            <GridItem pl="2" bg="white.300" area={"nav"}>
-              <Nav></Nav>
-            </GridItem>
-            <main>
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <>
-                      <GridItem pl="2" bg="primary.100" area={"header"}>
-                        <Header></Header>
-                      </GridItem>
-                      <GridItem pl="2" bg="white.300" area={"special"}>
-                        <Special></Special>
-                      </GridItem>
-                      <GridItem pl="2" bg="primary.100" area={"testimonials"}>
-                        <Testimonial></Testimonial>
-                      </GridItem>
-                      <GridItem pl="2" bg="secondary.300" area={"about"}>
-                        <AboutUs></AboutUs>
-                      </GridItem>
-                    </>
-                  }
-                />
-                <Route
-                  path="/reservation"
-                  element={
-                    <>
-                      <GridItem pl="2" bg="primary.100" area={"header"}>
-                        <Reservation></Reservation>
-                      </GridItem>
-                      <GridItem pl="2" bg="secondary.300" area={"special"}>
-                        <ContactForm></ContactForm>
-                      </GridItem>
-                      <GridItem pl="2" bg="primary.100" area={"testimonials"}>
-                        <Payment></Payment>
-                      </GridItem>
-                    </>
-                  }
-                />
-              </Routes>
-            </main>
-            <GridItem pl="2" bg="secondary.200" area={"footer"}>
-              <Footer></Footer>
-            </GridItem>
-          </Grid>
-        </body>
-      </Fragment>
+              gridTemplateRows={"auto auto 1fr 1fr 1fr auto"}
+              gridTemplateColumns={"1fr"}
+              h="-webkit-fit-content"
+              gap="0"
+              fontWeight="bold"
+            >
+              <GridItem pl="2" bg="white.300" area={"nav"}>
+                <Nav></Nav>
+              </GridItem>
+              <main>
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <>
+                        <GridItem pl="2" bg="primary.100" area={"header"}>
+                          <Header></Header>
+                        </GridItem>
+                        <GridItem pl="2" bg="white.300" area={"special"}>
+                          <Special></Special>
+                        </GridItem>
+                        <GridItem pl="2" bg="primary.100" area={"testimonials"}>
+                          <Testimonial></Testimonial>
+                        </GridItem>
+                        <GridItem pl="2" bg="secondary.300" area={"about"}>
+                          <AboutUs></AboutUs>
+                        </GridItem>
+                      </>
+                    }
+                  />
+                  <Route
+                    path="/reservation"
+                    element={
+                      <>
+                        <GridItem pl="2" bg="primary.100" area={"header"}>
+                          <Reservation></Reservation>
+                        </GridItem>
+                        <GridItem pl="2" bg="secondary.300" area={"special"}>
+                          <ContactForm></ContactForm>
+                        </GridItem>
+                        <GridItem pl="2" bg="primary.100" area={"testimonials"}>
+                          <Payment></Payment>
+                        </GridItem>
+                      </>
+                    }
+                  />
+                </Routes>
+              </main>
+              <GridItem pl="2" bg="secondary.200" area={"footer"}>
+                <Footer></Footer>
+              </GridItem>
+            </Grid>
+            <Alert></Alert>
+          </body>
+        </Fragment>
+      </AlertProvider>
     </ChakraProvider>
   );
 }
